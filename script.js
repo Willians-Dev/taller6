ce7c5386b180442ea344ca65f64b4b2362c015de
ESTUDIANTES <ESTUDIANTES@puce.edu.ec>
2024-12-17 20:31:47 -0500

    segundo commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// Select the button element
+// Select the elements
 const colorButton = document.getElementById('colorButton');
+const dynamicTitle = document.getElementById('dynamicTitle');
 
 // Function to generate a random color
 function getRandomColor() {
@@ -11,7 +12,20 @@ function getRandomColor() {
   return color;
 }
 
+// Function to generate a random title
+function getRandomTitle() {
+  const titles = [
+    "Welcome to the Color Party!",
+    "Change is Beautiful!",
+    "Enjoy Randomness!",
+    "Brighten Your Day!",
+    "Click and See the Magic!"
+  ];
+  return titles[Math.floor(Math.random() * titles.length)];
+}
+
 // Add a click event to the button
 colorButton.addEventListener('click', () => {
   document.body.style.backgroundColor = getRandomColor();
+  dynamicTitle.textContent = getRandomTitle();
 });
